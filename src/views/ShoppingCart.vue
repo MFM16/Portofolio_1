a5913e341e9b5327a40db19998d0d9b0ced81d40
MFM16 <farhanmaulidian16@gmail.com>
2021-09-21 20:54:08 +0700

    update

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -201,7 +201,7 @@ export default {
       axios
         .post("https://portomfm.000webhostapp.com/api/checkout", checkoutData)
         .then(() => this.$router.push("success"))
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(checkoutData, this.checkOut, err));
     },
   },
   mounted() {
